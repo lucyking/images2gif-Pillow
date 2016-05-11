c89f5ce59a8c0d0931a236c611359d670335ea0f
lucyking <joehisaishi1943@gmail.com>
2016-05-11 14:18:21 +0800

    init push

---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(
+    name="images2gif-Pillow",
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.0.0',
+    description="read and write animated gifs,for Pillow",
+
+    # Author details
+    author="lucyking",
+    author_email="joehisaishi1943@gmail.com",
+
+    # Choose your license
+    license='BSD',
+
+    # The project's main homepage.
+    url="https://github.com/lucyking/images2gif-Pillow",
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=['images2gif'],
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='gif images Pillow',
+)
